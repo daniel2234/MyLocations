f98bf808520b91f78535c02de1fdf68b8a4e11a0
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-06-16 11:56:28 -0400

    fixed pickedcategory segue

--- CategoryPickerViewController.swift
+++ CategoryPickerViewController.swift
@@ -58,7 +58,7 @@ class CategoryPickerViewController: UITableViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "PickCategory"{
+        if segue.identifier == "PickedCategory"{
             let cell = sender as! UITableViewCell
             if let indexPath = tableView.indexPathForCell(cell) {
                 selectedCategoryName = categories[indexPath.row]
